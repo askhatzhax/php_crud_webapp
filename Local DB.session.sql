7cae565ab3783bff2eaf104b4c175287d5528f95
askhatzhax <ash.zhaxybaev001@gmail.com>
2025-02-07 22:36:28 +0500

    comlpete 2

--- Local DB.session.sql
+++ Local DB.session.sql
@@ -1,3 +1,16 @@
+CREATE DATABASE IF NOT EXISTS users;
+use users;
+CREATE TABLE accounts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    company_name VARCHAR(150),
+    position VARCHAR(100),
+    phone1 VARCHAR(20),
+    phone2 VARCHAR(20),
+    phone3 VARCHAR(20)
+); 
 insert into accounts(id, first_name, last_name, email, company_name, position, phone1,phone2,phone3)
 values(2,"Wolter","White","Pinkman@gmail.com","Carwash","Boss",8788421,777777772,8777777773);
 
--- User.php
+++ User.php
@@ -1,12 +1,14 @@
 <?php
 //создаем класс
 class User {
+    // Создаем закрытый свойство класса для работы с бд
     private $db;
+    // Конструктор для создания объектов класса
     public function __construct($pdo) {
         $this->db = $pdo;
     }
 
-    // Получить список аккаунтов (с пагинацией)
+    // Метод для получения списка аккаунтов (с пагинацией)
     public function getAccounts($limit, $offset) {
         $stmt = $this->db->prepare("SELECT * FROM accounts LIMIT :limit OFFSET :offset");
         $stmt->bindValue(":limit", $limit, PDO::PARAM_INT);
@@ -15,7 +17,7 @@ class User {
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    // Добавить аккаунт
+    // Метод для добавления аккаунта
     public function addUser($first_name, $last_name, $email, $company, $position, $phones) {
         $stmt = $this->db->prepare("INSERT INTO accounts (first_name, last_name, email, company_name, position, phone1, phone2, phone3)
                                     VALUES (:first_name, :last_name, :email, :company, :position, :phone1, :phone2, :phone3)");
@@ -25,17 +27,19 @@ class User {
             ':email' => $email,
             ':company' => $company,
             ':position' => $position,
+            //Чтобы избежать ошибки Undefined array key проверяем на null
             ':phone1' => $phones[0] ?? null,
             ':phone2' => $phones[1] ?? null,
             ':phone3' => $phones[2] ?? null
         ]);
     }
 
-    // Удалить аккаунт
+    // Метод для удаления аккаунта
     public function deleteUser($id) {
         $stmt = $this->db->prepare("DELETE FROM accounts WHERE id = :id");
         return $stmt->execute([':id' => $id]);
     }
+    // Метод для обновления аккаунта
     public function updateUser($id, $first_name, $last_name, $email, $company, $position, $phones) {
         $stmt = $this->db->prepare("UPDATE accounts SET 
             first_name = :first_name, 
@@ -55,6 +59,7 @@ class User {
             ':email'     => $email,
             ':company'   => $company,
             ':position'  => $position,
+             //Чтобы избежать ошибки Undefined array key проверяем на null
             ':phone1'    => $phones[0] ?? null,
             ':phone2'    => $phones[1] ?? null,
             ':phone3'    => $phones[2] ?? null
